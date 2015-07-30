1cec61c9a125056603af317cca0401f47c638abd
unknown <jeremy01rx@yahoo.com.ph>
2015-07-31 00:43:03 +0800

    start new update

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
-	
+	$('.switch .switchBtn').on('click',function(){
+		$('.content').stop();
+		if($('.content').hasClass('activated')){
+			$('.content').removeClass('activated').animate({'margin-top':'-150px'})
+		}else{
+			$('.content').addClass('activated').animate({'margin-top':'0px'})
+		}
+	})
+	$('.crumbs').each(function(){
+		$(this).css({
+			'height':$(this).parent().height()+'px',
+			'line-height':parseInt($(this).parent().height()-10)+'px',
+		})
+	})
 })
