6a6d1b9b105b199d211a99a38c8d8b72ceba0bff
unknown <jeremy01rx@yahoo.com.ph>
2015-09-19 02:27:57 +0800

    new update

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,6 @@
 $(document).ready(function(){
-	$('.switch .switchBtn').on('click',function(){
-		$('.content').stop();
-		if($('.content').hasClass('activated')){
-			$('.content').removeClass('activated').animate({'margin-top':'-150px'})
-		}else{
-			$('.content').addClass('activated').animate({'margin-top':'0px'})
-		}
-	})
-	$('.crumbs').each(function(){
-		$(this).css({
-			'height':$(this).parent().height()+'px',
-			'line-height':parseInt($(this).parent().height()-10)+'px',
-		})
-	})
+	$('.accentContainer').height($(window).height());
+	// $(document).scroll(function(){
+	// 	$('.accentContainer').css('margin-top',$(document).scrollTop())
+	// })
 })
