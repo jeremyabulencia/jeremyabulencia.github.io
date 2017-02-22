a1887b1432f76e4c514a6d088c390dde3bfc81d1
Jeremy Abulencia <jeremy01rx@yahoo.com.ph>
2017-02-22 12:23:21 +0800

    new functions

---
+++ js/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+	
+	callbacks = $.Callbacks();
+
+	blurScroll = function(scrollTop){
+		scrollTop = (scrollTop > 200) ? 200 : scrollTop;
+		if(scrollTop <= 200){
+			$('#head').css({
+				'-webkit-filter': 'blur('+scrollTop/33+'px)',
+				'-moz-filter': 'blur('+scrollTop/33+'px)',
+				'-o-filter': 'blur('+scrollTop/33+'px)',
+				'-ms-filter': 'blur('+scrollTop/33+'px)',
+				'filter': 'blur('+scrollTop/33+'px)',
+			})
+		}
+	}
+
+	adjustContentPosition = function(){
+		$('#content').css({'top':$('#head').height() - 20})
+	}
+
+	callbacks.fire(blurScroll($(document).scrollTop()));
+	$(document).scroll(function(){
+		console.log($(this).scrollTop())
+		callbacks.fire(blurScroll($(this).scrollTop()))
+	})
+
+	callbacks.fire(adjustContentPosition());
+	$(window).resize(function(){
+		console.log($('#head').height())
+		callbacks.fire(adjustContentPosition())
+	})
+	
+})
