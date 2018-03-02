75e7b450606946264439592cbfa077f4b24463ba
Jeremy Abulencia <jabulencia@yondu.com>
2018-03-02 11:44:28 +0800

    mods and content

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
-$(document).ready(function(){
+callback = $.Callbacks();
+introSize = function(){
 	docHeight = $(window).height();
 	$('#header').css({'height':docHeight});
+}
+$(document).ready(function(){
+	callback.fire(introSize());
 });
+$(window).resize(function(){
+	callback.fire(introSize());
+})
