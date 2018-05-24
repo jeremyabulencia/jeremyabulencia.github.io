31520244f4a2a4a454c3356ff7ef805c8876a455
Jeremy Abulencia <jabulencia@yondu.com>
2018-05-24 12:47:09 +0800

    timeline and remove unnecc js

--- js/script.js
+++ js/script.js
@@ -7,11 +7,6 @@ $(document).ready(function(){
 	if($(window).width() > 696){
 		callback.fire(introSize());
 	}
-	p = new Date();
-	b = new Date('1990-03-15')
-	c = new Date(p-b);
-	c = parseInt(c/(1000*60*60*24*365));
-	console.log(c)
 });
 $(window).resize(function(){
 	if($(window).width() > 696){
